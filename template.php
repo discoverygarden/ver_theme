4e1ef989664d87bb9b13e74cb4ff9ac1ccaf8bc8
MorganDawe <mdawe@discoverygarden.ca>
2017-04-12 09:57:32 -0300

    Updated default styles and advanced search form alter.

--- template.php
+++ template.php
@@ -12,3 +12,15 @@ function ver_theme_form_islandora_solr_simple_search_form_alter(&$form, &$form_s
   $form['simple']['islandora_simple_search_query']['#attributes']['placeholder'] = t("Search this repository");
   $form['simple']['advanced_link'] = $link;
 }
+
+/**
+ * Implements hook_form_alter().
+ */
+function ver_theme_form_islandora_solr_advanced_search_form_alter(&$form, &$form_state, $form_id) {
+  foreach ($form['terms'] as $key => &$value) {
+    if (is_array($value)) {
+      $value['field']['#title_display'] = 'invisible';
+      $value['search']['#title_display'] = 'invisible';
+    }
+  }
+}
